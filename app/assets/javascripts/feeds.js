7c3c9d57c934c5c288daad850d1108e11ef7154c
Luke Reeves <luke.reeves@gmail.com>
2015-01-28 19:46:49 -0500

    Questioning life choices

--- app/assets/javascripts/feeds.js
+++ app/assets/javascripts/feeds.js
@@ -5,22 +5,22 @@
         var $cur = $('.current');
         var $next = $cur.next('.entry');
 
-        $('body').animate({
-          scrollTop: $next.offset().top
-        });
-
-        $cur.removeClass('current');
-        $next.addClass('current');
+        if($next.length) {
+          $(document).scrollTop($next.offset().top);
+          $cur.removeClass('current');
+          $next.addClass('current');
+        } else {
+          $(".mark-button").click();
+        }
       } else if (event.which == 107) {
         var $cur = $('.current');
         var $prev = $cur.prev('.entry');
 
-        $('body').animate({
-          scrollTop: $prev.offset().top
-        });
-
-        $cur.removeClass('current');
-        $prev.addClass('current');
+        if($prev.length) {
+          $(document).scrollTop($prev.offset().top);
+          $cur.removeClass('current');
+          $prev.addClass('current');
+        }
       }
     });
   })
