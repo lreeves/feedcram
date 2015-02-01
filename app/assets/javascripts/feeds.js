6524709c7343ee4fe8428d6b8f0793d0556744f5
Luke Reeves <luke.reeves@gmail.com>
2015-01-31 19:08:05 -0500

    Clean up feed JS

--- app/assets/javascripts/feeds.js
+++ app/assets/javascripts/feeds.js
@@ -2,42 +2,42 @@
   $(function () {
     $(document).ready(function() {
       // Select the first entry
-      var $first = $('.entry').first();
+      var first = $('.entry').first();
 
-      if($first.length) {
-        $first.addClass('current');
+      if(first.length) {
+        first.addClass('current');
       }
 
       // Set all timestamps to browser local time
       $.each($('span#timestamp'), function(i, element) {
-        rawDate = element.innerHTML.trim();
-        d = new Date(Date.parse(rawDate));
-        element.innerHTML = d.toLocaleString();
+        var rawDate = element.innerHTML.trim();
+        var date = new Date(Date.parse(rawDate));
+        element.innerHTML = date.toLocaleString();
       });
     });
 
     $(document).keypress(function(event) {
-      if(event.which == 106) {
-        var $cur = $('.current');
-        var $next = $cur.next('.entry');
+      var cur = $('.current');
 
-        if($next.length) {
-          $(document).scrollTop($next.offset().top);
-          $cur.removeClass('current');
-          $next.addClass('current');
+      if(event.which == 106) {
+        var next = cur.next('.entry');
+
+        if(next.length) {
+          $(document).scrollTop(next.offset().top);
+          cur.removeClass('current');
+          next.addClass('current');
         } else {
           $(".mark-button").click();
         }
       } else if (event.which == 107) {
-        var $cur = $('.current');
-        var $prev = $cur.prev('.entry');
+        var prev = cur.prev('.entry');
 
-        if($prev.length) {
-          $(document).scrollTop($prev.offset().top);
-          $cur.removeClass('current');
-          $prev.addClass('current');
+        if(prev.length) {
+          $(document).scrollTop(prev.offset().top);
+          cur.removeClass('current');
+          prev.addClass('current');
         }
       }
     });
-  })
+  });
 })(jQuery);
